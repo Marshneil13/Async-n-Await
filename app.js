befc695961bd6d2376cc48b3c2a1d5dadef0261e
marshnil_13 <btech10225.20@bitmesra.ac.in>
2021-10-31 23:01:49 +0530

    final commit

--- app.js
+++ app.js
@@ -62,4 +62,74 @@ login('marsh','myMeowIsTheBest')
 })
 // LOGGED IN!
 // app.js:57 WELCOME!
-//***********************************************
+//****************************************************************************
+
+const delayedColorChange = (color,delay) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        },delay)
+    })
+}
+
+// delayedColorChange('red',2000)
+//     .then(() => delayedColorChange('orange',2000))
+//     .then(() => delayedColorChange('yellow',2000))
+//     .then(() => delayedColorChange('green',2000))
+//     .then(() => delayedColorChange('blue',2000))
+//     .then(() => delayedColorChange('indigo',2000))
+//     .then(() => delayedColorChange('violet',2000))
+//     .catch((err) => console.error(err))
+
+    async function rainbow(){
+        await delayedColorChange('red', 1000)
+        console.log("Hey Red!");
+        await delayedColorChange('orange',1000)
+        console.log("Hey Orange!");
+        await delayedColorChange('yellow',1000)
+        console.log("Hey Yellow!");
+        await delayedColorChange('green',1000)
+        console.log("Hey Green!");
+        await delayedColorChange('blue',1000)
+        console.log("Hey Blue!");
+        await delayedColorChange('indigo',1000)
+        console.log("Hey Indigo!");
+        await delayedColorChange('violet',1000)
+        console.log("Hey Violet!");
+        return "ALL DONE!";
+    }
+
+    rainbow().then(() => console.log("END OF RAINBOW!"))
+
+    async function printRainbow(){
+        await rainbow();
+        console.log("END OF RAINBOW!");
+    }
+
+// ********************* handling errors in async functions *************************    
+    const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+async function makeTwoRequests(){
+    try{
+   let data1 = await fakeRequest('/page1')
+   let data2 = await fakeRequest('/page2')
+    console.log(data1);
+}catch(e){
+    console.log("CAUGHT AN ERROR");
+    console.log("error is: ",e);
+    }
+    
+}
+
